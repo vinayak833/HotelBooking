7e3a278e4e852567494b495ee0469acee99ecea6
VINAYAK <166581191+vinayak833@users.noreply.github.com>
2026-07-29 23:54:17 +0530

    Update Navbar.jsx

---
+++ Hotel-Booking/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Hotel Booking UI</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Hotels</li>
+        <li>Bookings</li>
+        <li>Profile</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
